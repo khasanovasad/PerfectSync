c35ec5b3724e1a142c890090154cdc0aad3bc4f5
Asadbek Khasanov <asadk@freightpop.com>
2024-09-21 22:04:51 +0500

    added Scrapper and LocationMatched code

---
+++ LocationMatcher/Program.cs
@@ -0,0 +1,282 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json;
+using System.Device.Gpio;
+using GeoCoordinatePortable;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace LocationMatcher;
+
+class Program
+{
+    public class DomTutData
+    {
+        public string Price { get; set; }
+        public string Class { get; set; }
+        public string Walls { get; set; }
+        public string Level { get; set; }
+        public string Rooms { get; set; }
+        public string Capacity { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Street { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string DomTutRating { get; set; }
+        public string Url { get; set; }
+        public string DistanceFromTheAmirTemurSquare { get; set; }
+    }
+
+    public class AllInData
+    {
+        public DomTutData BasicData {  get; set; }
+        public List<PlacesAround> PlacesAround { get; set; }
+    }
+
+    public class PlacesAround
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Longitude { set; get; }
+        public string Latitude { set; get; }
+        public string Distance { set; get; }
+
+    }
+
+    public static void NormalizeData(List<DomTutData> list)
+    {
+        list.ForEach(x =>
+        {
+            if (x.Class == "Biznes")
+            {
+                x.Class = "Business";
+            }
+
+            if (x.Class == "Qulaylik")
+            {
+                x.Class = "Comfort";
+            }
+
+            if (x.Walls == "G'isht")
+            {
+                x.Walls = "Bricks";
+            }
+
+            if (x.Walls == "gaz bloki")
+            {
+                x.Walls = "Aerated concrete";
+            }
+
+            if (x.Walls == "Keramoblok")
+            {
+                x.Walls = "Hollow bricks";
+            }
+
+            if (x.Walls == "Ko'pikli blok")
+            {
+                x.Walls = "Aerated concrete";
+            }
+
+            if (x.Walls == "shlakli blok")
+            {
+                x.Walls = "Aerated concrete";
+            }
+        });
+    }
+
+    public static async Task<AllInData> EnrichData(DomTutData data)
+    {
+        try
+        {
+            var latitude = data.Latitude;
+            var longitude = data.Longitude;
+            const string radius = "1000";
+
+            string query = @$"[out:json];
+(
+node[""amenity""=""school""](around:{radius},{latitude},{longitude});
+node[""railway""=""station""](around:{radius},{latitude},{longitude});
+node[""amenity""=""hospital""](around:{radius},{latitude},{longitude});
+node[""name""~""Korzinka""](around:{radius},{latitude},{longitude});
+way[""name""~""Korzinka""](around:{radius},{latitude},{longitude});
+relation[""name""~""Korzinka""](around:{radius},{latitude},{longitude});
+node[""name""~""Macro""](around:{radius},{latitude},{longitude});
+way[""name""~""Macro""](around:{radius},{latitude},{longitude});
+relation[""name""~""Macro""](around:{radius},{latitude},{longitude});
+node[""leisure""=""park""](around:{radius},{latitude},{longitude});
+way[""leisure""=""park""](around:{radius},{latitude},{longitude});
+relation[""leisure""=""park""](around:{radius},{latitude},{longitude});
+node[""landuse""=""recreation_ground""](around:{radius},{latitude},{longitude});
+way[""landuse""=""recreation_ground""](around:{radius},{latitude},{longitude});
+relation[""landuse""=""recreation_ground""](around:{radius},{latitude},{longitude});
+);
+out body;";
+
+            string apiUrl = "http://overpass-api.de/api/interpreter";
+            HttpContent content = new StringContent(query, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                response.EnsureSuccessStatusCode();
+
+                string responseData = await response.Content.ReadAsStringAsync();
+
+                using (JsonDocument document = JsonDocument.Parse(responseData))
+                {
+                    JsonElement root = document.RootElement;
+                    JsonElement elements = root.GetProperty("elements");
+
+                    var placesAround = new List<PlacesAround>();
+
+                    foreach (JsonElement element in elements.EnumerateArray())
+                    {
+                        string lat = string.Empty;
+                        string lon = string.Empty;
+                        string type = string.Empty;
+                        string name = string.Empty;
+                        string distance = string.Empty;
+
+                        try
+                        {
+                            lat = element.GetProperty("lat").GetDouble().ToString();
+                            lon = element.GetProperty("lon").GetDouble().ToString();
+                        }
+                        catch { }
+
+
+                        try
+                        {
+                            if (element.GetProperty("tags").TryGetProperty("railway", out var railway))
+                            {
+                                name = element.GetProperty("tags").GetProperty("alt_name").GetString();
+                            }
+                            else
+                            {
+                                name = element.GetProperty("tags").GetProperty("name").GetString();
+                            }
+                        }
+                        catch { }
+
+                        try
+                        {
+                            if (element.GetProperty("tags").TryGetProperty("amenity", out var school) && school.GetString() == "school")
+                            {
+                                type = "school";
+                            }
+                            if (element.GetProperty("tags").TryGetProperty("leisure", out var leisure) && leisure.GetString() == "park")
+                            {
+                                type = "park";
+                            }
+                            if (element.GetProperty("tags").TryGetProperty("shop", out var shop) && shop.GetString() == "supermarket")
+                            {
+                                type = "supermarket";
+                            }
+                        }
+                        catch { }
+
+                        if (!string.IsNullOrEmpty(lat) && !string.IsNullOrEmpty(lon))
+                        {
+                            var sCoord = new GeoCoordinate(Convert.ToDouble(data.Latitude), Convert.ToDouble(data.Longitude));
+                            var eCoord = new GeoCoordinate(Convert.ToDouble(lat), Convert.ToDouble(lon));
+                            distance = sCoord.GetDistanceTo(eCoord).ToString();
+                        }
+                        placesAround.Add(new PlacesAround { Latitude = lat, Longitude = lon, Name = name, Type = type, Distance = distance });
+                    }
+
+                    return new AllInData { BasicData = data, PlacesAround = placesAround.Where(x => !string.IsNullOrEmpty(x.Latitude) && !string.IsNullOrEmpty(x.Longitude)).ToList() };
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error occured: {e.Message}");
+            return new AllInData();
+        }
+    }
+
+    static async Task Main(string[] args)
+    {
+        string json1 = File.ReadAllText("./enriched_json_data2.json");
+        List<AllInData> datas1 = JsonConvert.DeserializeObject<List<AllInData>>(json1);
+
+        foreach (var data in datas1)
+        {
+            var sCoord = new GeoCoordinate(Convert.ToDouble(data.BasicData?.Latitude), Convert.ToDouble(data.BasicData?.Longitude));
+            var eCoord = new GeoCoordinate(41.3111, 69.2796);
+            var distance = sCoord.GetDistanceTo(eCoord).ToString();
+
+            if (data.BasicData != null)
+            {
+                data.BasicData.DistanceFromTheAmirTemurSquare = distance;
+            }
+        }
+
+        string toBeWritten = JsonConvert.SerializeObject(datas1, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText("./enriched_json_data3.json", toBeWritten);
+
+        /*
+        string json2 = File.ReadAllText("./normalized_json_data.json");
+        List<DomTutData> datas2 = JsonConvert.DeserializeObject<List<DomTutData>>(json2);
+
+        foreach (var data1 in datas1)
+        {
+            var found = datas2.FirstOrDefault(x => x.Url == data1.BasicData?.Url);
+
+            if (found != null)
+            {
+                data1.BasicData = found;
+            }
+        }
+
+        string toBeWritten = JsonConvert.SerializeObject(datas1, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText("./enriched_json_data2.json", toBeWritten);
+        */
+
+        /*
+        // removing duplicates
+        datas = datas.DistinctBy(x => x.Url).ToList();
+
+        NormalizeData(datas);
+        string ff = JsonConvert.SerializeObject(datas, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText("./normalized_json_data.json", ff);
+        return;
+
+        try
+        {
+            int maxConcurrency = 10; // Limit the number of concurrent tasks
+            SemaphoreSlim semaphore = new SemaphoreSlim(maxConcurrency);
+            var tasks = new List<Task<AllInData>>();
+
+            foreach (var data in datas)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    await semaphore.WaitAsync();
+                    try
+                    {
+                        Console.WriteLine(data.Url);
+                        return await EnrichData(data);
+                    }
+                    finally
+                    {
+                        semaphore.Release();
+                    }
+                }));
+
+            }
+
+            var allInData = await Task.WhenAll(tasks);
+
+            string toBeWritten = JsonConvert.SerializeObject(allInData, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText("./enriched_json_data.json", toBeWritten);
+        }
+        finally
+        {
+        }
+
+        */
+    }
+}
---
+++ Scrapper/Program.cs
@@ -0,0 +1,403 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using Newtonsoft.Json;
+using AngleSharp;
+using System.Text.RegularExpressions;
+
+namespace Scrapper;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // await DomTutHelper.GetDomTutLinks();
+        var obj = new ParseDomTutData();
+        await obj.ParseAllAvailableDomTutOpenings();
+    }
+}
+
+public class DomTutData
+{
+    public string Price { get; set; }
+    public string Class { get; set; }
+    public string Walls { get; set; }
+    public string Level { get; set; }
+    public string Rooms { get; set; }
+    public string Capacity { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public string Street { get; set; }
+    public string Longitude { get; set; }
+    public string Latitude { get; set; }
+    public string DomTutRating { get; set; }
+    public string DeveloperName { get; set; }
+    public string Url { get; set; }
+}
+
+public class ParseDomTutData
+{
+    private readonly List<string> _links = new List<string>();
+
+    public ParseDomTutData(string filePath = "./domtut_links.txt")
+    {
+        try
+        {
+            _links.AddRange(File.ReadAllLines(filePath).ToList());
+        }
+        catch { }
+    }
+
+    public async Task ParseAllAvailableDomTutOpenings()
+    {
+        int maxConcurrency = 15; // Limit the number of concurrent tasks
+        SemaphoreSlim semaphore = new SemaphoreSlim(maxConcurrency);
+
+        var list = new List<DomTutData>();
+        var tasks = new List<Task<DomTutData>>();
+
+        try
+        {
+            foreach (var link in _links)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    // Wait for the semaphore to allow this task to run
+                    await semaphore.WaitAsync();
+                    try
+                    {
+                        return await GetSingleDomTutDataAngleSharp(link);
+                    }
+                    finally
+                    {
+                        // Release the semaphore once the task is complete
+                        semaphore.Release();
+                    }
+                }));
+            }
+
+            var listList = await Task.WhenAll(tasks);
+            list.AddRange(listList);
+        }
+        finally
+        {
+            string json = JsonConvert.SerializeObject(list, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText("./json_domtut_data.json", json);
+        }
+    }
+    public async Task<DomTutData> GetSingleDomTutDataAngleSharp(string link)
+    {
+        var obj = new DomTutData { Url = link };
+
+        try
+        {
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(link);
+
+            // Set up AngleSharp configuration
+            var config = Configuration.Default;
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(req => req.Content(response));
+
+            // Price
+            try
+            {
+                var priceElement = document.QuerySelector("span.price");
+                obj.Price = priceElement?.TextContent.Trim();
+            }
+            catch { }
+
+            // Developer Name
+            try
+            {
+                var developerElement = document.QuerySelector("span.author");
+                obj.DeveloperName = developerElement?.TextContent.Split(':')[1].Trim();
+            }
+            catch { }
+
+            // Class
+            try
+            {
+                var classElement = document.QuerySelector("div:contains('Uy-joy sinfi:') + div");
+                obj.Class = classElement?.TextContent.Trim();
+            }
+            catch { }
+
+            // Walls
+            try
+            {
+                var wallsElement = document.QuerySelector("div:contains('Material:') + div");
+                obj.Walls = wallsElement?.TextContent.Trim();
+            }
+            catch { }
+
+            // Level
+            try
+            {
+                var levelElement = document.QuerySelector("div:contains('Qavat:') + div");
+                obj.Level = levelElement?.TextContent.Trim().Split(' ').Last().Trim();
+            }
+            catch { }
+
+            // Rooms
+            try
+            {
+                var roomsElement = document.QuerySelector("h1[itemprop='name']");
+                obj.Rooms = roomsElement?.TextContent[0].ToString();
+            }
+            catch { }
+
+            // Capacity
+            try
+            {
+                var capacityElement = document.QuerySelector("h1[itemprop='name']");
+                obj.Capacity = capacityElement?.TextContent.Split(',')[1].Trim();
+            }
+            catch { }
+
+            // City
+            try
+            {
+                var cityElement = document.QuerySelector("b:contains('Shahar:') + span");
+                obj.City = cityElement?.TextContent.Trim();
+            }
+            catch { }
+
+            // Province
+            try
+            {
+                var provinceElement = document.QuerySelector("b:contains('Tuman:') + span");
+                obj.Province = provinceElement?.TextContent.Trim();
+            }
+            catch { }
+
+            // Street
+            try
+            {
+                var streetElement = document.QuerySelector("b:contains('Manzil:') + span");
+                obj.Street = streetElement?.TextContent.Trim();
+            }
+            catch { }
+
+            // Latitude & Longitude
+            try
+            {
+                var anchorTags = document.QuerySelectorAll("a");
+                var coordinatePattern = new Regex(@"\d{2}\.\d+,\d{2}\.\d+");
+
+                foreach (var anchorTag in anchorTags)
+                {
+                    if (anchorTag.TextContent.Trim() == "Google Xaritalarda oching")
+                    {
+                        // Use the regex to find the coordinates in the href value
+                        var match = coordinatePattern.Match(anchorTag.OuterHtml);
+
+                        if (match.Success)
+                        {
+                            string coordinates = match.Value;
+                            string[] latLong = coordinates.Split(',');
+                            obj.Latitude = latLong[0];
+                            obj.Longitude = latLong[1];
+                            break;
+                        }
+                    }
+                }
+            }
+            catch { }
+
+            // Rating
+            try
+            {
+                var ratingElement = document.QuerySelector("div.rating-score");
+                obj.DomTutRating = ratingElement?.TextContent.Split('/')[0].Trim() + "/5";
+            }
+            catch { }
+
+            Console.WriteLine(obj.Url);
+            return obj;
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occurred: " + ex.Message);
+            return obj;
+        }
+    }
+
+    public DomTutData GetSingleDomTutData(ChromeDriver driver, string link)
+    {
+        driver.Navigate().GoToUrl(link);
+
+        DomTutHelper.WaitForPageLoad(driver);
+        DomTutHelper.WaitForAjax(driver);
+
+        var obj = new DomTutData();
+
+        obj.Url = link;
+        try
+        {
+            obj.Price = driver.FindElement(By.CssSelector("span.price")).Text;
+        }
+        catch { }
+
+        try
+        {
+            obj.DeveloperName = driver.FindElement(By.CssSelector("span.author")).Text.Split(':')[1].Trim();
+        }
+        catch { }
+
+        try
+        {
+            obj.Class = driver.FindElement(By.XPath("//div[@class='row prop-tag']/div[2]")).Text;
+        }
+        catch { }
+
+        try
+        {
+            obj.Walls = driver.FindElement(By.XPath("//div[@class='row prop-tag']/div[10]")).Text;
+        }
+        catch { }
+
+        try
+        {
+            obj.Level = driver.FindElement(By.XPath("//div[@class='row prop-tag']/div[14]")).Text.Split(' ').Last();
+        }
+        catch { }
+
+        try
+        {
+            obj.Rooms = driver.FindElement(By.XPath("//h1[@itemprop='name']")).Text[0].ToString();
+        }
+        catch { }
+
+        try
+        {
+            obj.Capacity = driver.FindElement(By.XPath("//h1[@itemprop='name']")).Text.Split(',')[1].ToString().Trim();
+        }
+        catch { }
+
+        try
+        {
+            obj.City = driver.FindElement(By.XPath("//b[contains(text(),'Shahar:')]/following-sibling::span")).Text;
+        }
+        catch { }
+
+        try
+        {
+            obj.Province = driver.FindElement(By.XPath("//b[contains(text(),'Tuman:')]/following-sibling::span")).Text;
+        }
+        catch { }
+
+        try
+        {
+            obj.Street = driver.FindElement(By.XPath("//b[contains(text(),'Manzil:')]/following-sibling::span")).Text;
+        }
+        catch { }
+
+        #region l&l
+        try
+        {
+            IWebElement googleMapsLink = driver.FindElement(By.XPath("//a[contains(@href, 'google.com/maps')]"));
+
+            string hrefValue = googleMapsLink.GetAttribute("href");
+
+            Uri uri = new Uri(hrefValue);
+            string query = uri.Query;
+            var queryParams = System.Web.HttpUtility.ParseQueryString(query);
+            string coordinates = queryParams["daddr"];
+
+            string[] latLong = coordinates.Split(',');
+            obj.Latitude = latLong[0];
+            obj.Longitude = latLong[1];
+        }
+        catch { }
+        #endregion
+        #region rating
+        try
+        {
+            IWebElement ratingElement = driver.FindElement(By.CssSelector("div.rating-score"));
+
+            string ratingText = ratingElement.Text;
+
+            obj.DomTutRating = ratingText.Split('/')[0].Trim() + "/5";
+        }
+        catch { }
+        #endregion
+
+        return obj;
+    }
+}
+
+public class DomTutHelper
+{
+    public static async Task GetDomTutLinks()
+    {
+        const string mainUrl = "https://domtut.uz/uz/filter/search?negotiate=false&p={0}";
+
+        var listOfLinks = new List<string>();
+
+        var driver = new ChromeDriver();
+
+        int lastIndex = 0;
+
+        try
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                var list = new List<string>();
+                var currentUrl = String.Format(mainUrl, i + 1);
+
+                driver.Navigate().GoToUrl(currentUrl);
+
+                WaitForPageLoad(driver);
+                WaitForAjax(driver);
+                WaitForJavaScriptCondition(driver, "return window.customVariable == true;");
+
+                var elements = driver.FindElements(By.TagName("a"));
+                foreach (var element in elements)
+                {
+                    list.Add(element.GetAttribute("href"));
+                }
+
+                list = list.Where(x => !String.IsNullOrEmpty(x) && x.StartsWith("https://domtut.uz/uz/nedvizhimost/")).ToList();
+                listOfLinks.AddRange(list);
+
+                lastIndex = i;
+            }
+        }
+        catch { /* nothing */ }
+        finally
+        {
+            driver.Quit();
+
+            Console.WriteLine("LastIndex: " + lastIndex);
+            File.WriteAllLines("./domtut_links.txt", listOfLinks);
+        }
+
+    }
+
+    public static void WaitForPageLoad(IWebDriver driver)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").ToString() == "complete");
+    }
+
+    public static void WaitForAjax(IWebDriver driver)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        wait.Until(driver => (bool)((IJavaScriptExecutor)driver).ExecuteScript("return jQuery.active == 0"));
+    }
+
+    public static void WaitForJavaScriptCondition(IWebDriver driver, string script)
+    {
+        try
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            wait.Until(driver => (bool)((IJavaScriptExecutor)driver).ExecuteScript(script));
+        }
+        catch (Exception ex)
+        {
+            // nothing 
+        }
+    }
+}
