62ea8ea84939681584430f426671141f27f1c4e1
Bekhzod Irgashev <irgashevbehzod@gmail.com>
2024-09-21 15:32:48 +0500

    fix: updated prettier

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -2,11 +2,11 @@ import './App.css'
 import Map from './components/map/Map'
 
 function App() {
-	return (
-		<div className='App'>
-			<Map />
-		</div>
-	)
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  )
 }
 
 export default App
